0f1c5d087546cec3387e77928a2e855d3e33427c
Natalie Wong <wong.lt.natalie@gmail.com>
2025-10-03 23:15:24 -0400

    Users list style horizontal alignment fixup

--- src/users/components/UsersList.jsx
+++ src/users/components/UsersList.jsx
@@ -11,7 +11,7 @@ function UsersList(props) {
     };
     
     return (
-        <ul className="list-none my-0 mx-auto p-0 w-[90%] max-w-[50rem] flex content-center flex-wrap">
+        <ul className="list-none my-0 mx-auto p-0 w-[90%] max-w-[50rem] flex justify-center flex-wrap">
             {props.items.map(user => 
                 <UserItem 
                     key={user.id}
