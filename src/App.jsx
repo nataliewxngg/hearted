b61b290b7c4533ff557662d112ef96074a444f06
nataliewxngg <wong.lt.natalie@gmail.com>
2025-10-04 10:38:39 -0400

    Add and style header

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,22 @@ import {
   Routes,
   Navigate,
 } from "react-router-dom";
+
 import Users from "./users/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Users />} exact />
-        <Route path="/places/new" element={<NewPlace />} exact />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} exact />
+          <Route path="/places/new" element={<NewPlace />} exact />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
     </Router>
   )
 }
