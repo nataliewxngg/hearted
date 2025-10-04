e28a01292828d9820bd61e36831d708c11246222
Natalie Wong <wong.lt.natalie@gmail.com>
2025-10-03 22:21:16 -0400

    Add dummy UsersList into Users page

---
+++ src/users/components/UserItem.jsx
@@ -0,0 +1,7 @@
+function UserItem() {
+    return (
+        <h1>User Item</h1>
+    );
+}
+
+export default UserItem;
--- src/users/pages/Users.jsx
+++ src/users/pages/Users.jsx
@@ -1,5 +1,16 @@
+import UsersList from "../components/UsersList"
+
 function Users() {
-    return <h2>Users Page</h2>
+    const USERS = [
+        {
+            id: "u1",
+            image: "https://i.pinimg.com/736x/b8/80/73/b88073a07bf14ea3c5e423ecdb2c3d29.jpg",
+            name: "natalie",
+            places: 3
+        }
+    ];
+
+    return <UsersList items={USERS} /> // MUST HAVE ITEMS PROP
 }   
 
 export default Users;
